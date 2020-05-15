19015c71076b1515dcea421f5cfb996cf070293b
Thiago183 <56850405+Thiago183@users.noreply.github.com>
2020-05-15 20:15:50 -0300

    Add files via upload

---
+++ UI-DS-EAD/BLL-DS-EAD/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_DS_EAD;
+using DAL_DS_EAD;
+
+namespace BLL_DS_EAD
+{
+    public class Class1
+    {
+    }
+}
